7369f2e6868eb2e935aac158ca00979598c48ece
Silvano Coriani <scoriani@microsoft.com>
2022-07-14 08:27:45 +0000

    add a div

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -40,6 +40,7 @@ export default function Home({
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
+      <h1><div>my test</div></h1>
       <Grid variant="filled">
         {products.slice(0, 3).map((product: any, i: number) => (
           <ProductCard
